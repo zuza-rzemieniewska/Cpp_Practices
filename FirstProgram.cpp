a989734102097e98acc243c36546d03e1c072401
zuza-rzemieniewska <zuza.horabik@gmail.com>
2021-01-15 19:34:25 +0100

    Add First Program

---
+++ FirstProgram.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+
+    string your_name = "Zuza";
+
+    cout << "Hello world!" << endl;
+    //cout << "What's your name?";
+    //cin >> your_name;
+    cout << "Hello " << your_name << "!" <<endl;
+
+    return 0;
+    //comment
+    /* multiline
+    comment */
+    
+}
